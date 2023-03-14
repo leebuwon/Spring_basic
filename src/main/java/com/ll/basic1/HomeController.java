0b46aed65598738f840645fda174fa8dff652b51
bw1611 <bw1611@naver.com>
2023-03-14 11:44:24 +0900

    Increase 추가

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
+    private int sum;
+
+    public HomeController() {
+        this.sum = 0;
+    }
+
     @GetMapping("/home/main")
     @ResponseBody // 아래 메서드를 싱행한 후 그 리턴값을 응답으로 준다.
     public String showMain1(){
@@ -14,14 +20,21 @@ public class HomeController {
     }
 
     @GetMapping("/home/main2")
-    @ResponseBody // 아래 메서드를 싱행한 후 그 리턴값을 응답으로 준다.
+    @ResponseBody
     public String showMain2(){
         return "반갑습니다.";
     }
 
     @GetMapping("/home/main3")
-    @ResponseBody // 아래 메서드를 싱행한 후 그 리턴값을 응답으로 준다.
+    @ResponseBody
     public String showMain3(){
         return "즐거웠습니다.";
+    }
+
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public String showIncrease(){
+        sum++;
+        return "응답 : " + sum;
     }
 }
