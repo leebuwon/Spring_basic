3794983ec20dca3c068b8dbf86e27e26558dd11d
bw1611 <bw1611@naver.com>
2023-03-14 11:54:59 +0900

    RequestParam

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.basic1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -36,5 +37,15 @@ public class HomeController {
     public String showIncrease(){
         sum++;
         return "응답 : " + sum;
+    }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    // @RequestParam 의 의미
+    // int a는 쿼리스트링에서 a 파라미터의 값을 얻은 후 정수화한 값을 더한다.
+    // /plus?a=100&&b=200
+    public int showPlus(@RequestParam(defaultValue = "100") int a,
+                        @RequestParam int b){
+        return a + b;
     }
 }
