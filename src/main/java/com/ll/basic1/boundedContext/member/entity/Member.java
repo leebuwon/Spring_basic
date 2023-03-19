2737155e3dbe27d0a3e7b42aaa2ddb74e1baa683
bw1611 <bw1611@naver.com>
2023-03-19 20:52:10 +0900

    member를 JPA통하여 관리

--- src/main/java/com/ll/basic1/boundedContext/member/entity/Member.java
+++ src/main/java/com/ll/basic1/boundedContext/member/entity/Member.java
@@ -1,22 +1,29 @@
 package com.ll.basic1.boundedContext.member.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@AllArgsConstructor
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
 @Getter
 @ToString
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Member {
 
-    private static long lastId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+    @Column(unique = true)
     private String username;
     private String password;
 
-    static {
-        lastId = 0;
-    }
-
-    public Member(String username, String password){
-        this(++lastId, username, password);
-    }
 }
--- src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
+++ src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
@@ -11,17 +11,16 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class MemberService {
-
     private final MemberRepository memberRepository;
 
     public RsData tryLogin(String username, String password) {
-        Member member = memberRepository.findByUsername(username);
+        Member member = memberRepository.findByUsername(username).orElse(null);
 
-        if (member == null){
-            return RsData.of("F-2", "%s는 존재하지 않는 회원입니다.".formatted(username));
+        if (member == null) {
+            return RsData.of("F-2", "%s(은)는 존재하지 않는 회원입니다.".formatted(username));
         }
 
-        if (!member.getPassword().equals(password)){
+        if (!member.getPassword().equals(password)) {
             return RsData.of("F-1", "비밀번호가 일치하지 않습니다.");
         }
 
@@ -29,10 +28,10 @@ public class MemberService {
     }
 
     public Member findByUsername(String username) {
-        return memberRepository.findByUsername(username);
+        return memberRepository.findByUsername(username).orElse(null);
     }
 
     public Member findById(long id) {
-        return memberRepository.findById(id);
+        return memberRepository.findById(id).orElse(null);
     }
 }
